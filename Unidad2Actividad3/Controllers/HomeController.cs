b8624a93faa6e1692cccbb0519baedf7285dcccc
Daniela Alejandra Oviedo Torres <60947400+DanielaOviedo@users.noreply.github.com>
2020-09-22 19:46:36 -0500

    .

---
+++ Unidad2Actividad3/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Unidad2Actividad3.Models;
+
+namespace Unidad2Actividad3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            villancicosContext context = new villancicosContext();
+            var villancicos = context.Villancico.OrderBy(x=>x.Nombre);
+            return View(villancicos);
+        }
+        public IActionResult Villancico(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            villancicosContext context = new villancicosContext();
+            var v = context.Villancico.FirstOrDefault(x => x.Id==id);
+            if (v == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            return View(v);
+        }
+    }
+}
